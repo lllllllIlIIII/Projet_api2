a102f0649820a52cac81c608ae227af875702234
samed baritci <samed.baritci@condorcet.be>
2026-05-19 01:23:48 +0200

    add : Program.cs

--- Program.cs
+++ Program.cs
@@ -1,26 +1,37 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Projet_api2.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Account/Login"; 
+        options.AccessDeniedPath = "/Error";  
+        options.ExpireTimeSpan = TimeSpan.FromHours(2);
+        options.SlidingExpiration = true;
+    });
+
+builder.Services.AddScoped<UserService>(provider => new UserService(connectionString!));
+builder.Services.AddScoped<ProjetService>(provider => new ProjetService(connectionString!)); 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
+app.UseStatusCodePagesWithReExecute("/Error", "?statusCode={0}");
 app.UseHttpsRedirection();
-
+app.UseStaticFiles();
 app.UseRouting();
-
+app.UseAuthentication(); 
 app.UseAuthorization();
-
-app.MapStaticAssets();
-app.MapRazorPages()
-   .WithStaticAssets();
+app.MapRazorPages();
 
 app.Run();
