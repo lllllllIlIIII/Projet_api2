0f7fef504dbbf118bbe7275365d036c8da36ca0c
samed baritci <samed.baritci@condorcet.be>
2026-05-18 17:46:19 +0200

    add : model projetmembre

--- Models/ProjetMembre.cs
+++ Models/ProjetMembre.cs
@@ -1,6 +1,13 @@
 ﻿namespace Projet_api2.Models;
+using System.ComponentModel.DataAnnotations;
 
 public class ProjetMembre
 {
-    
+    public int UserId { get; set; }
+    public int ProjectId { get; set; }
+    [Required(ErrorMessage = "Le role dans le projet est obligatoire.")]
+    [StringLength(100, ErrorMessage = "Le role ne doit pas dépasser 100 caractères.")]
+    public string RoleDansProjet { get; set; } = string.Empty;
+    public string? NomComplet { get; set; }
+    public string? Email { get; set; }
 }
